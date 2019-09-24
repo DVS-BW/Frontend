266b598b59541375cefc18da26da7de7cfcbab28
Wais A Almakaleh <wais5542@gmail.com>
2019-09-24 11:58:25 -0400

    restarted

--- src/Components/Debt/debt.js
+++ src/Components/Debt/debt.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { withFormik, Field, Form } from 'formik';
 
-function Debt (){
 
-    return(
-        <div className='debt'>
-        <h1>Debt</h1>
-        <input type='text' name='cost' placeholder='Cost'/>
-        
-        </div>
-    )
-}
-
-export default Debt;
+const BasicExample = () => (
+    <div>
+       
+        <Formik
+           
+        />
+    </div>
+);
