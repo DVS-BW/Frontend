168d8f49e4991d582681c7514e046a8f3c0e00b8
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-09-25 12:51:24 -0400

    Login and register forms created. Both are working and posting new users to the backend.

--- src/Components/Nav/nav.js
+++ src/Components/Nav/nav.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Signup from '../Signup';
+import Login from '../Login';
 
 const Nav = () => {
     return (
+        <Router>
         <div className='navbar'>
             <div>
                 LOGO
             </div>
             <ul>
-                <li>Log In</li>
-                <li>Sign-Up</li>
+                <Link to="/login"><li>Log In</li></Link>
+                <Link to="/signup"><li>Sign-Up</li></Link>
                 <li>About</li>
                 <li>Support</li>
              </ul>   
+             
         </div>
+        <Route path='/signup' component={Signup}/>
+        <Route path='/login' component={Login}/>
+        </Router>
     )
 }
 
