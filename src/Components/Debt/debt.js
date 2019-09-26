86234f0c6dacbb507907518a04a1532462b92505
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-09-26 12:58:00 -0400

    Debt component is hooked up to redux.

--- src/Components/Debt/debt.js
+++ src/Components/Debt/debt.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import { Form, Field, withFormik, Formik } from 'formik';
-import axios from 'axios';
+import { Form, Field, Formik } from 'formik';
 // import * as Yup from 'yup';
 
 
 function Debt(props) {
-    const [debt, setDebt] = useState([]);
+    const [debt, setDebt] = useState({});
 
     const changeHandler = event => {
 
@@ -19,41 +18,45 @@ function Debt(props) {
                 isDebtEditing: true,
                 [event.target.name]: val
             })};
-            console.log(event.target.value);
     }
 
 
 
     return (
         <Formik className='debt'> 
-        
-        
         <Form onSubmit={(e) => { props.submitHandler(e, debt) }} >
         <h1> Debt </h1>
+
         <div className='container'>
+
         <label>Credit Card {" "}
         <Field
             type='text'
             className='input'
-            name='credit card'
+            name='credit_card'
+            value={debt.credit_card}
             placeholder='$ Credit Card'
             onChange={changeHandler}/>
                         
-            </label>
+        </label>
+
         <label>Personal Loan {" "}
         <Field
             className='input'
             type='text'
-            name='Personal Loan'
+            name='personal_loan'
+            value={debt.personal_loan}
             placeholder='$ Personal Loan'
             onChange={changeHandler}/>
                         
             </label>
+
         <label> Car Loan {" "}
         <Field
             className='input'
             type='text'
-            name='Personal Loan'
+            name='car_loan'
+            value={debt.car_loan}
             placeholder='$ Car Loan'
             onChange={changeHandler}/>
                         
@@ -63,7 +66,8 @@ function Debt(props) {
         <Field
             className='input'
             type='text'
-            name='Mortgage'
+            name='mortgage'
+            value={debt.mortgage}
             placeholder='$ Mortgage'
             onChange={changeHandler}/>
                         
@@ -72,23 +76,26 @@ function Debt(props) {
         <Field
             type='text'
             className='input'
-            name='Student loans'
+            name='student_loans'
+            value={debt.student_loans}
             placeholder='$ Student Loans'
             onChange={changeHandler}/>
             </label>
+
             <label> Other {" "}
             <Field
             className='input'
             type='text'
             name='other'
+            value={debt.other}
             placeholder='$ other'
             onChange={changeHandler}/>
                         
             </label>
             
             </div>
-                <h4> NEXT </h4>
-                <h1>total Cost is $0</h1> 
+                <button type='submit' className='submitBTN'>Next</button>
+
             </Form>
         </Formik>
     )
